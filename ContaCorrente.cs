42ce56bd8ae3518faa257d52329ec68940ec0537
Perkles <lucasbatistasuzart@gmail.com>
2022-06-01 22:36:02 -0300

    Classe ContaCorrente

---
+++ ContaCorrente.cs
@@ -0,0 +1,17 @@
+﻿namespace bytebank
+{
+    public class ContaCorrente
+    {
+       public string titular;
+       public string conta;
+       public int numero_agencia;
+       public string nome_agencia;
+       public double saldo;
+        public bool verificador;
+    }
+
+
+
+
+
+}
--- Program.cs
+++ Program.cs
@@ -1,15 +1,61 @@
-﻿Console.WriteLine("Boas Vindas ao seu banco, ByteBank");
+﻿using bytebank;
 
-string titular = "Andre Bessa";
-string conta = "10123-x";
-int numero_agencia = 23;
-string nome_agencia = "Agencia Central";
-double saldo = 93.50;
+Console.WriteLine("Bem vindo ao seu banco,  Bytebank ");
 
-Console.WriteLine("Titular: " + titular);
-Console.WriteLine("Conta: " + conta);
-Console.WriteLine("Número Agencia: " + numero_agencia);
-Console.WriteLine("Nome da Agencia: " + nome_agencia);
-Console.WriteLine("Saldo: " + saldo);
+ContaCorrente conta1 = new ContaCorrente();
+conta1.titular = "André Silva";
+conta1.conta = "110123-X";
+conta1.numero_agencia = 123;
+conta1.nome_agencia = "Agência Central";
+conta1.saldo = 100;
+//------------------------------------------------
+ContaCorrente conta2 = new ContaCorrente();
+conta2.titular = "Amanda Nogueira";
+conta2.conta = "10106-X";
+conta2.numero_agencia = 321;
+conta2.nome_agencia = "Agência Central";
+conta2.saldo = 100;
+
+//----------------------------------
+
+ContaCorrente conta3 = new ContaCorrente();
+//conta2.titular = "Amanda Nogueira";
+//conta2.conta = "10106-X";
+//conta2.numero_agencia = 321;
+//conta2.nome_agencia = "Agência Central";
+//conta2.saldo = 100;
+
+
+//-------------------------------------------------------------------
+
+Console.WriteLine("Titular :" + conta1.titular);
+Console.WriteLine("Conta :" + conta1.conta);
+Console.WriteLine("Número Agência :" + conta1.numero_agencia);
+Console.WriteLine("Nome Agência :" + conta1.nome_agencia);
+Console.WriteLine("Saldo: " + conta1.saldo);
+//------------------------------------------------------------------
+
+
+Console.WriteLine("Titular :" + conta2.titular);
+Console.WriteLine("Conta :" + conta2.conta);
+Console.WriteLine("Número Agência :" + conta2.numero_agencia);
+Console.WriteLine("Nome Agência :" + conta2.nome_agencia);
+Console.WriteLine("Saldo: " + conta2.saldo);
+//=======================================================
+
+Console.WriteLine("Titular :" + conta3.titular);
+Console.WriteLine("Conta :" + conta3.conta);
+Console.WriteLine("Número Agência :" + conta3.numero_agencia);
+Console.WriteLine("Nome Agência :" + conta3.nome_agencia);
+Console.WriteLine("Saldo: " + conta3.saldo);
+Console.WriteLine("Verificador: " + conta3.verificador);
+
+//ContaCorrente conta = new ContaCorrente();
+//Console.WriteLine($"Titular da conta: {conta.titular}");
+//Console.WriteLine($"Número da conta: {conta.conta}");
+//Console.WriteLine($"Agência: {conta.nome_agencia}");
+//Console.WriteLine($"Saldo R$ {String.Format("{0:0.00}", conta.saldo)}");
+
+
 
 Console.ReadKey();
