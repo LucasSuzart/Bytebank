1983308c255370aab3fe0242ef880819be68099d
Perkles <lucasbatistasuzart@gmail.com>
2022-06-08 01:03:09 -0300

    Classes Add

--- ContaCorrente.cs
+++ ContaCorrente.cs
@@ -4,16 +4,15 @@ namespace bytebank
 {
     public class ContaCorrente
     {
-        public string titular;
+        public Cliente titular;
         public string conta;
         public int numero_agencia;
         public string nome_agencia;
-        public double saldo;
-        
-        
+        private double saldo;
+
         public bool Sacar(double valor)
         {
-            if (saldo< valor)
+            if (saldo < valor)
             {
                 return false;
             }
@@ -28,6 +27,44 @@ namespace bytebank
             }
         }
 
+        public void Depositar(double valor)
+        {
+            if (valor < 0)
+            {
+                return;
+            }
+            saldo = saldo + valor;
+        }
+
+        public bool Transferir(double valor, ContaCorrente destino)
+        {
+            if (saldo < valor)
+            {
+                return false;
+            }
+            if (valor < 0)
+            {
+                return false;
+            }
+            else
+            {
+                saldo = saldo - valor;
+                destino.saldo = destino.saldo + valor;
+                return true;
+            }
+        }
+
+        public void DefinirSaldo(double valor)
+        {
+            if (valor < 0)
+            {
+                return;
+            }
+            else
+            {
+                saldo = saldo + valor;
+            }
+        }
+    }
 
     }
-}
